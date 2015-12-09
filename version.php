0411cc256f4cd2d1c3a71423daae006ff39f08ac
Pavel Sokolov <pavel.m.sokolov@gmail.com>
2015-12-09 13:31:38 +0300

    Fix version for Moodle 3.x

--- version.php
+++ version.php
@@ -26,8 +26,7 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$module->version  = 2012080801;  // The current module version (Date: YYYYMMDDXX)
-$module->requires = 2010080300;  // Requires this Moodle version
-$module->cron     = 0;           // Period for cron to check this module (secs)
-
-
+$plugin->version  = 2012080801;  // The current module version (Date: YYYYMMDDXX)
+$plugin->requires = 2010080300;  // Requires this Moodle version
+$plugin->cron     = 0;           // Period for cron to check this module (secs)
+$plugin->component = 'mod_labelcollapsed';
